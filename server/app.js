7782831832ef0d96fafa83e78bd5f4052243b947
otitoju1 <john.o@allprimetech.com>
2022-02-11 20:25:37 -0800

    edit server

--- server/app.js
+++ server/app.js
@@ -1,41 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('./index.routing');
+const bodyParser = require('body-parser')
+const app = express()
+const routes = require('./index.routing')
+const cors = require("cors");
 const config = require('./config');
-
-const app = express();
-const PORT = process.env.PORT || 6000;
-
-const uri = `mongodb+srv://${config.db_username}:${config.password}@${config.cluster}.mongodb.net/${config.dbname}?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', routes)
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next()
-});
-
-// app.get("/", (_req, res) => {
-//     res.send(`<h1>Application server on ${PORT}`)
-// })
-
-// app.get("*", (_req, res) => {
-//     res.send(`<h1>Route not found on ${PORT}`)
-// })
-
-try {
-    app.listen(PORT, ()=> {
-        console.log(`Application started on ${PORT}`);
-        //DATABASE CONNECTION
-        mongoose.connect(uri);
-    })
-} catch (error) {
-    console.error(`Error occured: ${error.message}`);
-}
+//PORT
+const port = process.env.PORT || 7000;
+app.listen(port,() =>{
+    console.log(`Application is listening to ${port}`)
+})
+app.get('*', (req,res) => {
+    res.json(`404 ERROR, PAGE NOT FOUND`)
+})
+//DATABASE CONNECTION
+const uri = `mongodb+srv://${config.db_username}:${config.password}@${config.cluster}.mongodb.net/${config.dbname}?retryWrites=true&w=majority`;
+mongoose.connect(uri);
--- server/config.js
+++ server/config.js
@@ -3,6 +3,7 @@ module.exports = {
     "db_username": "otitoju",
     "password": "abcd1234",
     "cluster": "cluster0.joqfv",
-    "dbname": "data_collections"
+    "dbname": "data_collections",
+    "local_DB": "mongodb://localhost:27017/food_recipe"
 }
 //SECRET CONFIGURATION
